0a89092df147a63b9231044c4f152e20096f3e9b
Francesco Lana <franklana@hotmail.it>
2025-06-24 12:40:50 +0200

    Update climate.py

--- esphome/components/madoka_vam/climate.py
+++ esphome/components/madoka_vam/climate.py
@@ -12,15 +12,10 @@ MadokaVam = madoka_vam_ns.class_(
 )
 
 CONFIG_SCHEMA = (
-    climate.CLIMATE_SCHEMA.extend(
-        {
-            cv.GenerateID(): cv.declare_id(MadokaVam),
-        }
-    )
+    climate.climate_schema(MadokaVam)
     .extend(ble_client.BLE_CLIENT_SCHEMA)
     .extend(cv.polling_component_schema("10s"))
 )
-
 
 async def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
